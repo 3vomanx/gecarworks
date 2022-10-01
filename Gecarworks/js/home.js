82fedafe6de01722c8225ea21f7fe9f084e25add
3vomanx <97153803+3vomanx@users.noreply.github.com>
2022-10-01 07:40:25 -0700

    Add files via upload

---
+++ Gecarworks/js/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".switch").click(function(){
+        $("body").toggleClass("dark-mode");
+    if($("body").hasClass("dark-mode")){
+        $(".switch").attr("name", "sunny-outline");
+    }else{
+        $(".switch").attr("name", "moon-outline"); 
+    }
+  });
+});
